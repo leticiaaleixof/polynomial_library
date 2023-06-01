7e4e0f559954af68af83307deccf0d80ba8277d1
leticiaaleixof <134062205+leticiaaleixof@users.noreply.github.com>
2023-06-01 16:10:18 -0300

    Add eval at point x

--- src/polynomials.py
+++ src/polynomials.py
@@ -1,1 +1,9 @@
 
+    '''
+    Evaluates the value of the polynomial at a given x.
+    '''
+    def eval(self, x):
+        value_at_x = 0
+        for degree in self.coef_at_degree:
+            value_at_x += self.coef_at_degree[degree] * x ** degree
+        return value_at_x
