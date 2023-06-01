7fa9332fac5bbea95b57c36956b74b2d07f8f649
leticiaaleixof <134062205+leticiaaleixof@users.noreply.github.com>
2023-06-01 16:16:14 -0300

    Create class called Polynomial

--- src/polynomials.py
+++ src/polynomials.py
@@ -1,3 +1,11 @@
+class Polynomial:
+    '''
+    The initialization recieves a dict where the
+    indexes are non-negative degrees and the values
+    are the coeficients
+    '''
+    def __init__(self, coef_at_degree):
+        self.coef_at_degree = coef_at_degree
 
     '''
     Evaluates the value of the polynomial at a given x.
