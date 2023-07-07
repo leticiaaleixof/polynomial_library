dcbfb139cefa1c1b9bd898550cd4c7c3aff32ece
leticiaaleixof <134062205+leticiaaleixof@users.noreply.github.com>
2023-07-07 12:25:22 -0300

    Update polynomials.py
    add function that transforms a polynomial in string form into a dictionary.

--- src/polynomials.py
+++ src/polynomials.py
@@ -52,3 +52,56 @@ class Polynomial:
     if polynomial[len(polynomial)-1] == ' ':
       polynomial = polynomial[:len(polynomial)-1]
     return polynomial
+  
+
+  def dict_polynomial(self, polynomial = None):
+    '''Given a polynomial in string format, the code will
+    return a dictionary representation of the polynomial.
+    Args:
+        polynomial (str): Polynomial string in the format
+        'a_n*x^n + a_(n-1)*x^(n-1) + ... + a_1*x + a_0'.
+    Returns:
+        dict: Dictionary representation of the polynomial,
+        where the keys are the exponents and the values
+        are the coefficients.'''
+    if polynomial == None:
+      polynomial = self.polynomial
+    # Preprocess the string to standardize the format
+    polynomial_str = polynomial.replace('+x^', '+1:')
+    if polynomial_str[:2] == 'x^':
+      polynomial_str = polynomial_str.replace('x^', '1:')
+    elif polynomial_str[:1] == 'x':
+      polynomial_str = polynomial_str.replace('x', '1:1')
+    polynomial_str = polynomial_str.replace('-x^', '-1:')
+    polynomial_str = polynomial_str.replace('x^', ':')
+    polynomial_str = polynomial_str.replace('x', ':1')
+    terms = []
+    p_list = polynomial_str.split('+')
+    for negative in p_list:
+      p_list = negative.replace('-', ' -')
+      p_list = p_list.split(' ')
+      terms += p_list
+    if terms[0] == '':
+      del terms[0]
+    # add degree zero to the constant term:
+    final_term = terms[len(terms)-1]
+    if ':' not in final_term:
+      final_term = final_term.replace(final_term, final_term +':0')
+      terms[len(terms)-1] = final_term
+    dictionary = {}
+    for term in terms:
+      term_parts = []
+      term_parts.append(term)
+      '''Convert the coefficient to float or int, depending on its value
+      If the coefficient contains a decimal point, it is converted to float
+      Otherwise, it is converted to int.'''
+      for part in term_parts:
+        term_parts = part.split(':')
+        coefficient = term_parts[0]
+        degree = term_parts[1]
+        if float(coefficient)%1 == 0:
+          dictionary[int(degree)] = int(float(coefficient))
+        else:
+          dictionary[int(degree)] = float(coefficient)
+    return dictionary
+
